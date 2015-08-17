4f741d6630fbc68d8752491ad138d0f08a029178
Christopher Haglund <christopher.haglund@sweco.se>
2015-08-17 17:20:14 +0200

    adapt to ember 2.0 helper

--- addon/helpers/get-name.js
+++ addon/helpers/get-name.js
@@ -1,5 +1,7 @@
 import Em from 'ember';
 
-export default Em.Handlebars.makeBoundHelper(function(obj, field) {
+export default Em.Helper.helper(function(params) {
+	var obj = params[0];
+	var field = params[1];
   return obj.get(field);
 });
